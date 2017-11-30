423f3bd1f508f194c644c4c45dfab45851eadc1f
Michael Gurley <m-gurley@northwestern.edu>
2017-11-30 14:48:23 -0500

    cose #4 Create Code model.

---
+++ db/migrate/20171129153742_create_codes.rb
@@ -0,0 +1,8 @@
+class CreateCodes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :codes do |t|
+      t.string  :code,  null: false
+      t.timestamps null: false
+    end
+  end
+end
