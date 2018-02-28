e3ed33ee39fed267749e961f6550765d1fe190b7
Michael Gurley <m-gurley@northwestern.edu>
2018-02-28 17:04:15 -0500

    Add pagination to BatchHealthPro show page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, params.permit(:sort, :direction, :search, :last_name, :first_name, :record_id, :email, :assignment_status, :registration_status).merge({ sort: column, direction: direction }), { class: css_class }
+    link_to title, params.permit(:status, :sort, :direction, :search, :last_name, :first_name, :record_id, :email, :assignment_status, :registration_status).merge({ sort: column, direction: direction }), { class: css_class }
   end
 
   def validation_errors?(object, field_name)
