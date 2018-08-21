d13a9cf9275bd8aa5e2a819fc7642eafeb5feac3
Michael Gurley <m-gurley@northwestern.edu>
2018-08-21 10:19:56 -0400

    Monit files

--- config/monit/production/delayed_job_start_all_of_us_helper.sh
+++ config/monit/production/delayed_job_start_all_of_us_helper.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-cd /var/www/apps/disburser/current
-/usr/local/rvm/bin/rvm-shell -c 'RAILS_ENV=production /var/www/apps/disburser/current/bin/delayed_job --pid-dir=/var/www/apps/disburser/shared/pids/ start'
+cd /var/www/apps/all-of-us-helper/current
+#RAILS_ENV=staging /var/www/apps/all-of-us-helper/current/bin/delayed_job --pid-dir=/var/www/apps/all-of-us-helper/shared/pids/ start
+cd /var/www/apps/all-of-us-helper/current
+/usr/local/rvm/bin/rvm-shell -c 'RAILS_ENV=production /var/www/apps/all-of-us-helper/current/bin/delayed_job --pid-dir=/var/www/apps/all-of-us-helper/shared/pids/ start'
--- config/monit/production/delayed_job_stop_all_of_us_helper.sh
+++ config/monit/production/delayed_job_stop_all_of_us_helper.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cd /var/www/apps/disburser/current
-/usr/local/rvm/bin/rvm-shell -c 'RAILS_ENV=production /var/www/apps/disburser/current/bin/delayed_job --pid-dir=/var/www/apps/disburser/shared/pids/ stop'
+cd /var/www/apps/all-of-us-helper/current
+/usr/local/rvm/bin/rvm-shell -c 'RAILS_ENV=production /var/www/apps/all-of-us-helper/current/bin/delayed_job --pid-dir=/var/www/apps/all-of-us-helper/shared/pids/ stop'
