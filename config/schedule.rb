ee4097d8ef80c1bbadb29f47eebbfbbd0c031fd2
Michael Gurley <m-gurley@northwestern.edu>
2018-09-13 12:42:32 -0400

    close #64 Schedule job to synch patients from REDCap every 5 minutes.

--- config/schedule.rb
+++ config/schedule.rb
@@ -24,11 +24,18 @@ set :output, {:error => 'log/whenever_error.log', :standard => 'log/whenever.log
 
 case environment
   when 'production'
+    # every 5.minutes do # 1.minute 1.day 1.week 1.month 1.year is also supported
+    #   rake "redcap:assign_invitation_codes"
+    # end
+    #
     every 5.minutes do # 1.minute 1.day 1.week 1.month 1.year is also supported
-      rake "redcap:assign_invitation_codes"
+      rake "redcap:synch_patients"
     end
   when 'staging'
+    # every 5.minutes do # 1.minute 1.day 1.week 1.month 1.year is also supported
+    #   rake "redcap:assign_invitation_codes"
+    # end
     every 5.minutes do # 1.minute 1.day 1.week 1.month 1.year is also supported
-      rake "redcap:assign_invitation_codes"
+      rake "redcap:synch_patients"
     end
 end
