7ef63387b356693218f872fee87723f402d82c00
Michael Gurley <m-gurley@northwestern.edu>
2018-12-13 11:34:13 -0500

    references #104 Debugging Study Tracker api problem.

--- app/models/api_log.rb
+++ app/models/api_log.rb
@@ -1,11 +1,13 @@
 class ApiLog < ActiveRecord::Base
   def self.create_api_log(url, payload, response, error, system)
-    ApiLog.create!(
-                  url: url,
-                  payload: payload,
-                  response: response,
-                  error: error,
-                  system: system
-                  )
+    ApiLog.transaction(requires_new: true) do
+      ApiLog.create!(
+                    url: url,
+                    payload: payload,
+                    response: response,
+                    error: error,
+                    system: system
+                    )
+      end
   end
 end
