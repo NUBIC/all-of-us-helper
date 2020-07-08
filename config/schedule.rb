349c8cda05b0568056dbf6b573f4be7ce67bfdb8
Michael Gurley <m-gurley@northwestern.edu>
2020-07-08 06:38:42 -0400

    references #108 Schedule health pro api rake tasks.

--- config/schedule.rb
+++ config/schedule.rb
@@ -28,6 +28,14 @@ case environment
     #   rake "health_pro_api_migrate:migrate"
     # end
 
+    every :day, at: '4:00am' do # Use any day of the week or :weekend, :weekday
+      rake "health_pro_api:rotate_service_account_key"
+    end
+
+    every :day, at: '4:05am' do # Use any day of the week or :weekend, :weekday
+      rake "health_pro_api:import_api"
+    end
+
     every 1.hour do # 1.minute 1.day 1.week 1.month 1.year is also supported
       rake "redcap:synch_patients"
     end
