1d02b55571c572174df24eceeb1f565e14e2b215
Michael Gurley <m-gurley@northwestern.edu>
2020-07-10 05:21:23 -0400

    Update daily schedule.

--- config/schedule.rb
+++ config/schedule.rb
@@ -28,13 +28,9 @@ case environment
     #   rake "health_pro_api_migrate:migrate"
     # end
 
-    every :day, at: '6:12am' do # Use any day of the week or :weekend, :weekday
-      rake "health_pro_api:import_api"
-    end
-
-    every :day, at: '3:55am' do # Use any day of the week or :weekend, :weekday
-      rake "maintenance:expire_batch_health_pros"
-    end
+    # every :day, at: '6:12am' do # Use any day of the week or :weekend, :weekday
+    #   rake "health_pro_api:import_api"
+    # end
 
     every :day, at: '4:00am' do # Use any day of the week or :weekend, :weekday
       rake "health_pro_api:rotate_service_account_key"
@@ -42,6 +38,10 @@ case environment
 
     every :day, at: '4:05am' do # Use any day of the week or :weekend, :weekday
       rake "health_pro_api:import_api"
+    end
+
+    every :day, at: '3:55am' do # Use any day of the week or :weekend, :weekday
+      rake "maintenance:expire_batch_health_pros"
     end
 
     every 1.hour do # 1.minute 1.day 1.week 1.month 1.year is also supported
